d5a204f808894556aa96f22383cdf4541d088fc0
ananasF <155177572+ananasF@users.noreply.github.com>
2023-12-29 16:48:26 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+'use strict'
+var speed = +prompt('מה המהירות שאתה נוסע ?')
+undefined
+if (speed > 100){
+    alert('אם תיסע מהר יתפוס אותך שוטר')
+} else {
+    alert('אם תיסע לעט יעשו ממך סלט סע מהר')
+}
